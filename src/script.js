e6465c9f8acd349cc7227dbb0707ca9468437655
Simon <62551180+simonagleev@users.noreply.github.com>
2022-11-06 12:15:06 +0300

    nothing

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { Color, Group, Vector3 } from 'three'
+import { Vector3 } from 'three'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 
